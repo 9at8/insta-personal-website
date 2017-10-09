dadd669cd38a3b08be2447e7c1ef2e34c49e6d70
Aditya Thakral <aditya.1998thakral@gmail.com>
2017-10-08 22:27:31 -0400

    Add GA

--- client/index.js
+++ client/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import ReactGA from 'react-ga'
 
 import App from './app';
 
 import './index.css';
 
+ReactGA.initialize('UA-92216360-2')
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render((
-    <Router>
+    <Router onUpdate={logPageView}>
       <App/>
     </Router>
   ), document.getElementById('app')
